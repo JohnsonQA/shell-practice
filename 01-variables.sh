a3d01d85646c46a667a9323a6e5945e7f5a56713
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-17 15:44:59 +0530

    Variables

---
+++ 01-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#variables - Which holds the values of different data types and can be used anywhere in the code dynamically
+
+PERSON1=John
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2, How you doing?"
+echo "$PERSON2:: Hey $PERSON1!! I am doing great. How about you?"
+echo "$PERSON1:: Doing great. When are you going to Mars?"
+echo "$PERSON2:: May be end of the year. You wanna join in?"
+echo "$PERSON1:: Yes ofcourse!! would love to visit Mars with you :)"
+
