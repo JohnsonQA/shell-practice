f0d4adab074003cfa9ebe39e6417884fd1b96521
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-17 15:49:49 +0530

    Variables

--- 01-variables.sh
+++ 01-variables.sh
@@ -2,8 +2,12 @@
 
 #variables - Which holds the values of different data types and can be used anywhere in the code dynamically
 
-PERSON1=John
-PERSON2=Musk
+#Ways of using Varibales
+#Initilaize varibale at global level and use the variable keyword using $ sign followed by varibale name everywhere in the code
+#Passing as an arguments during run time. Just by defining the values as $1 and $2 in the variables
+
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1:: Hey $PERSON2, How you doing?"
 echo "$PERSON2:: Hey $PERSON1!! I am doing great. How about you?"
