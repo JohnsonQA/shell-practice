00a38a95031e3b7ed3f7a11d0ac9e04f7a920b40
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-17 17:02:35 +0530

    Variables

--- 03-data-types.sh
+++ 03-data-types.sh
@@ -7,7 +7,7 @@ TIMESTAMP=$(date)   #Get the output of date and store it in Timestamp.
 NUMBER1=100
 NUMBER2=300
 
-SUM=$(($NUMBER1+$NUMBER2))  #() performs an action. Here addition. (()) outer braces which stores the output and pass it to $ so that output prints
+SUM=$(($NUMBER1+$NUMBER2))  #() performs an action on any operators. Here addition. (()) outer braces which stores the output and pass it to $ so that output prints
 echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
 echo "Script executed at: $TIMESTAMP"
 
---
+++ 05-special-varibales.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "The file name of the script: $0"
+echo "Arguments size passed to a script: $#"
+echo "All varibales passed to the script: $@"
+echo "All varibale passed in double quotes: $*"
+echo "Present working Directory: $PWD"
+echo "Present Home Directory: $HOME"
+echo "Present user who running the script: $USER"
+echo "PID of current script: $$"
+sleep 10 &
+echo "PID of last command running in the background: $!"
