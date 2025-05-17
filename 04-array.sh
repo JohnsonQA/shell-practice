1761f0c364be11fb6e194689a256eb539d5d0b20
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-17 16:27:15 +0530

    Variables

--- 04-array.sh
+++ 04-array.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+#Arrays - It holds multiple values in a single varibale and its fixed in size
+
 movies=("court" "HIT3" "Dragon" "Thandel")
 
 echo "First movie in the list: ${movies[0]}"  #We can define variables in this way as well ${varibalename}
-echo "The size of the movies list is: ${movies}"
+echo "The size of the movies list is: ${#movies[@]}"
+echo "All the movies in the list are: ${movies[@]}"
