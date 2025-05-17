894b92f1ac662aca8b3fecd1bb4f4502c0d8d14a
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-17 19:25:21 +0530

    Variables

--- 07-installation.sh
+++ 07-installation.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
+#userId - id -u or sudo id -u
 
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR: Run the script with root access"
+    exit 1 #exit status other than 0 is a failure
+else
+    echo "Running with root access..."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL not installed. Going to install..."
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "MYSQL installed Sucessfully..."
+    else
+        echo "MYSQL installation Failed"
+        exit 1
+else
+    echo "MYSQL already installed... Nothing to do"
+fi
