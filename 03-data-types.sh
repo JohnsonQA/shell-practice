b3dca4379193fee2d090a486d82f00a93ff71712
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-17 16:24:02 +0530

    Variables

--- 03-data-types.sh
+++ 03-data-types.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+#In shell, no need to declare the data type explicitly. As per its intelligence it automatically converts it to specific type.
+#Ex: Num1=John Num2=300 Sum=$(($NUM1+$NUM2)) then in NUM1 it takes as 0 and add it and gives the result as 300
+
 TIMESTAMP=$(date)   #Get the output of date and store it in Timestamp. 
 NUMBER1=100
 NUMBER2=300
 
-SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))  #() performs an action. Here addition. (()) outer braces which stores the output and pass it to $ so that output prints
 echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
 echo "Script executed at: $TIMESTAMP"
+
+
+
---
+++ 04-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+movies=("court" "HIT3" "Dragon" "Thandel")
+
+echo "First movie in the list: ${movies[0]}"  #We can define variables in this way as well ${varibalename}
+echo "The size of the movies list is: ${movies}"
