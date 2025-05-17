12bc6150305e9d62dd1b0442ffe84068f8a9456e
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-17 16:00:33 +0530

    Variables

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Hide the value using variables for more authnetication in scenarios like Otp's or PIN
+
+echo "Enter the PIN:: "
+read PIN  # PIN is varibale
+echo "Entered PIN is: $PIN"
