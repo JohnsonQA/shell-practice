eae88b45a8a40504d5bc2b8d7b36056eea1c9e7b
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-18 15:39:19 +0530

    Installing packages

--- 05-special-varibales.sh
+++ 05-special-varibales.sh
@@ -10,4 +10,5 @@ echo "Present Home Directory: $HOME"
 echo "Present user who running the script: $USER"
 echo "PID of current script: $$"
 sleep 10 &
+echo "Previous exit status: $?"
 echo "PID of last command running in the background: $!"
