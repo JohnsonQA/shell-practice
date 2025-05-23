c1107de770ee928067d3cf25863d1f9889c13948
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-24 00:10:11 +0530

    function interactions

--- script1.sh
+++ script1.sh
@@ -4,6 +4,6 @@ echo "Hey, Good Morning"
 
 echo "PID of script 1 is: $$"
 
-# ./script2.sh   # this is one way of calling script 2 in script 1
+ ./script2.sh   # this is one way of calling script 2 in script 1
 
-source ./script2.sh # another way. Here we no need to give exeute permissions to script 2 whereas for above we should
+# source ./script2.sh # another way. Here we no need to give exeute permissions to script 2 whereas for above we should
