f6f14b10dbf001e5456536bcc14adca4cbadf383
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-24 15:28:59 +0530

    Reading Lines

--- 13-whileLoop.sh
+++ 13-whileLoop.sh
@@ -2,7 +2,7 @@
 
 #Problem-statement - Read the lines in a file
 
-while IFS= read -r line
+while  read -r line   #It reads the line in 12-set.sh . IFS - Internal Field separator
 do
     echo $line
 done < 12-set.sh
