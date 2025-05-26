18e755e28a3d60144ba7a158352d07159c084537
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-05-26 21:36:51 +0530

    Backup Script

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -3,12 +3,19 @@
 #Get the disk utilization of a mounted files and send an automated email to the user
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=1 #Usually in companies will have 75%
+DISK_THRESHOLD=1 #usually in projects we use 75%
+MSG=""
 
 
 while IFS= read line
 do
-    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)  #it will read line by line and gives the utilization number
     PARTITION=$(echo $line | awk '{print $7F}')
-    echo "$PARTITION: $USAGE"
-done <<< "$DISK_USAGE"
+
+    if [ $DISK_USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk usage of $PARTITION: $USAGE \n"      #Since loop resets the everyline + will concantenate all lines. In shell \n and -e uses separte them in lines
+    fi
+done <<< $DISK_USAGE
+
+echo -e $MSG
