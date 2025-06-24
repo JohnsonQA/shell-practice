40c6a2d48c1b5b0d16707faaee24ae59a5dff87b
JohnsonQA <Johnson.kudumula09@gmail.com>
2025-06-24 20:45:52 +0530

    sg module test

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -5,13 +5,12 @@
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=1 #usually in projects we use 75%
 MSG=""
-IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)  #It will provide the current IP address of the running server
 
 while IFS= read line
 do
-    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1) 
-    PARTITION=$(echo $line | awk '{print $7F}')
+    USAGE=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk '{print $7F}')  /dev 
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
         MSG+="High Disk usage of $PARTITION: $USAGE % <br>"   #HTML br to break the line   #+ concats the previous output with new output and \n and -e works as new line and prints out 
